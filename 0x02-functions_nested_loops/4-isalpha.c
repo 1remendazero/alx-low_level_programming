8d9e786e06a4f8f0c3400648f715fa08c4431683
Chukwuemeka <jamesmulla600@gmail.com>
2022-10-20 10:15:08 -0700

    Write a function that checks for alphabetic character

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,8 +3,7 @@
 /**
  * _isalpha - checks for alphabets
  * @c: character to be checked
- *
- * Return: 1if character ia a letter,0 otherwise
+ * Return: 1 if character is a letter, 0 otherwise
  */
 int _isalpha(int C)
 {
